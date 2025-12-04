189354faca65c98bf7b342a3b75fca120f41de9f
de-soot <78423238+de-soot@users.noreply.github.com>
2025-12-04 11:58:53 +0000

    Update knapsack.c
    Signed-off-by: de-soot <78423238+de-soot@users.noreply.github.com>

--- knapsack.c
+++ knapsack.c
@@ -55,7 +55,7 @@ int *findSubset(int *set, int *subset, int length, int target, int sum, int inde
 	if(sum == target) { // cannot add more elements
 		// denote end of subset (last non-zero element is indexed at takeCount - 1)
 		// denoted with zero because optimal subset should not have zeros (which contribute nothing to the sum)
-		if(takeCount < length) {
+		if(takeCount < length) { // do not denote if subset is full
 			subset[takeCount] = 0;
 		}
 		
