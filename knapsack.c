5d2c44124c148d45435562eb436b4efd44dc28e3
wwc <78423238+de-soot@users.noreply.github.com>
2025-12-05 20:20:04 +0800

    edited some comments in knapsack.c and included takeCount checks in findSubset()'s if-conditions

--- knapsack.c
+++ knapsack.c
@@ -51,33 +51,34 @@ void parseSet(int *set, char **argv, int length) {
 	}
 }
 
-// find the first valid subset in an array of numbers using recursion
+// find the first valid subset matching the exact target value using recursion
 // will return the subset with the lowest possbile number of elements if
 // the original set is sorted in ascending order.
-// will skip all first
+// will skip all elements first
 int *findSubset(int *set, int *subset, int length, int target, int sum, int index, int takeCount) {
-	if(sum == target) { // found a valid subset
-		// denote end of subset (last non-zero element is indexed at takeCount - 1)
-		// because optimal subset should not have zeros (which contribute nothing to the sum) if target is non-zero
+	if(takeCount > 0 && sum == target) { // found non-empty valid subset
+		// denote end of subset (last element is indexed at takeCount - 1)
+		// subset should not have zeros (which contribute nothing to the sum) if target is non-zero
 		if(takeCount < length) { // do not denote if subset is full
 			subset[takeCount] = 0;
 		}
 		
 		return subset;
 	} else if(index < length) {
-		// skip current number in set
+		// skip current number
 		int *skip = findSubset(set, subset, length, target, sum, index+1, takeCount);
 		if(skip != 0) { return skip; }
-		
+
 		int num = set[index];
 
-		// take current number in set
-		if(num != 0) { // optimal subset should take no zeros
-			subset[takeCount] = num; // latest previously added element is indexed at the current takeCount - 1
+		// take current number
+		// target == 0 XNOR num == 0
+		if((target == 0) == (num == 0)) { // subset should take no zeros for non-zero targets
+			subset[takeCount] = num; // append new element to subset
 			int *take = findSubset(set, subset, length, target, sum + num, index+1, takeCount+1);
 			if(take != 0) { return take; }
 		}
 	}
 
-	return 0; // if reached the end of set and could not find a valid subset, end current traversal
+	return 0; // if no non-empty valid subset found, end traversal
 }
